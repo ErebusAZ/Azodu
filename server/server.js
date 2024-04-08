0f2a9267787f15d5f81e067d599d892d8246a643
Erebus_AZ <azodu@proton.me>
2024-04-08 16:10:19 -0400

    Increase limit for GET requests

--- server/server.js
+++ server/server.js
@@ -47,7 +47,7 @@ app.use(express.json()); // This line is added to parse JSON request bodies
 // Define a rate limit rule with a custom key generator
 const getRateLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 200, // limit each IP to 200 requests per windowMs
+  max: 500, // limit each IP to 500 requests per windowMs
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
   message: 'Too many requests, please try again later.',
