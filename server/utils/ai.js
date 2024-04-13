115b8d18bf281e4d6859888fb640d9e18bd8e790
Erebus_AZ <azodu@proton.me>
2024-04-13 13:31:31 -0400

    Remove unicode emotes from AI comments

--- server/utils/ai.js
+++ server/utils/ai.js
@@ -78,8 +78,9 @@ function removeQuotesFromString(inputString) {
 const commentsCache = {};
 
 function removeUnwantedPatterns(inputString) {
-  // Remove occurrences of "..." or ".."
-  return inputString.replace(/\.\.+/g, '');
+  // Remove occurrences of "..." or ".." and Unicode emojis
+  // The regex for emojis covers ranges of common emoji characters
+  return inputString.replace(/\.\.+/g, '').replace(/[\u{1F600}-\u{1F64F}\u{1F300}-\u{1F5FF}\u{1F680}-\u{1F6FF}\u{1F700}-\u{1F77F}\u{1F780}-\u{1F7FF}\u{1F800}-\u{1F8FF}\u{1F900}-\u{1F9FF}\u{1FA00}-\u{1FA6F}\u{1FA70}-\u{1FAFF}]/gu, '');
 }
 
 async function generateAIComment(title, summary, model, post_id) {
