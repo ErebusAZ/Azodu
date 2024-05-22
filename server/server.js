711bcb3688fc70319ee129bed8cc4e8781d860db
Erebus_AZ <azodu@proton.me>
2024-05-22 11:37:10 -0400

    Generate AI comments every 3 min

--- server/server.js
+++ server/server.js
@@ -149,7 +149,9 @@ const cache = {
 
 // the following should probably be staggered if multiple servers are running
 const UPDATE_VOTES_AND_PINNED_CACHE_INTERVAL = Math.floor(Math.random() * 51 + 10) * 1000; // how quickly to fetch all posts and update votes, random # between 10 and 50 sec
-const COMMENT_GENERATION_INTERVAL_MS = Math.floor(Math.random() * (7 - 3 + 1) + 3) * 60000; // between 3 and 7 minutes
+
+// following only happens on master
+const COMMENT_GENERATION_INTERVAL_MS = 3 * 60000; // 3 min
 const FREQUENCY_TO_CREATE_POSTS_FROM_EXTERNAL_FETCH = 60000 * 10; // 10 min
 
 
